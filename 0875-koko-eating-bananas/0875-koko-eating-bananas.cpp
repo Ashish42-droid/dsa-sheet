347d4273662ffa033d5b4f7fdb914111f0a4bbe2
Ashish42-droid <ashkumar91980@gmail.com>
2026-06-28 00:00:04 +0530

    Time: 3 ms (95.27%), Space: 23 MB (12.95%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-    bool possible (vector<int>&piles,int h ,long long guess){
-        int hours =0;
-      for (int banana:piles){
-        int q = banana / guess;
-        int r = banana  % guess;
-        hours+=q;
-        if (r!=0)
-        hours++;
-        if (hours>h)//bahut jyada slow kha rahi hai isliye low ko  badha denge
-         return true;
-       
-        
-      }
-        return false;
+    long long possible(vector<int>& piles, long long guess) {
+        long long hours = 0;
+        for (int banana : piles) {
+            long long q = banana / guess;
+            long long r = banana % guess;
+            hours += q;
+            if (r != 0) hours++;
+        }
+        return hours;
     }
+
     int minEatingSpeed(vector<int>& piles, int h) {
-    long long n= piles.size();
-    long long low=1;
-     long long high = *max_element(piles.begin(),piles.end());
-    
+        long long low = 1;
+        long long high = *max_element(piles.begin(), piles.end());
+        int res = -1;
 
-    while(low<=high){
-        long long guess=( low+high)/2;
-         if (possible(piles,h,guess))
-         
-           low =guess+1;//bahut jyada slow kha rahi hai isliye low ko  badha denge
-         else 
-        high = guess -1;//bahut jyada fast kha rahi hai isliye high ko kam kar denge
+        while (low <= high) {
+            long long guess = (low + high) / 2;
+            long long hours = possible(piles, guess);
 
-    }
-    return low;
+            if (hours > h) {
+                // too slow, increase speed
+                low = guess + 1;
+            } else {
+                // valid speed, try slower
+                res = guess;
+                high = guess - 1;
+            }
+        }
+        return res;
     }
 };
