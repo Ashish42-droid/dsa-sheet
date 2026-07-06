8d49bef10d0392b370f1965a39ea1abebbcb6087
Ashish42-droid <ashkumar91980@gmail.com>
2026-07-06 18:07:59 +0530

    Time: 2 ms (26.97%), Space: 8.5 MB (30.25%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -2,45 +2,56 @@ class Solution {
 public:
     string reorganizeString(string s) {
         unordered_map<char,int> mp;
-        for (char c : s)
-            mp[c]++;
-        
-        int n=s.size();
-        for(auto it : mp ){
-            if (it.second>(n+1)/2)
-            return  "";
+
+        for(int i = 0; i < s.size(); i++) {
+            mp[s[i]]++;
         }
+
+        int n = s.size();
+        for(auto it : mp) {
+            if(it.second > (n + 1) / 2)
+                return "";
+        }
+
         priority_queue<pair<int,char>> pq;
-        for (auto it = mp.begin();it!=mp.end();it++){
+
+        for(auto it = mp.begin(); it != mp.end(); it++) {
             pq.push({it->second, it->first});
         }
-        string res =  "";
-        int seats=0;
-        while (!pq.empty()){
+
+        string res = "";
+
+        while(!pq.empty()) {
+
             pair<int,char> p = pq.top();
             pq.pop();
-            if (seats==0||res[seats-1]!=p.second){
+
+            if(res.empty() || res.back() != p.second) {
+
                 res.push_back(p.second);
                 p.first--;
-                seats++;
-                if (p.first>0)
-                pq.push(p);
-            }else{
-                if (pq.empty())
-                return  "";
-                pair<int,char> q=pq.top();
+
+                if(p.first > 0)
+                    pq.push(p);
+            }
+            else {
+
+                if(pq.empty())
+                    return "";
+
+                pair<int,char> p2 = pq.top();
                 pq.pop();
-                res.push_back(q.second);
-                q.first--;
-                seats++;
-                if (q.first>0)
-                pq.push(q);
 
-               pq.push(p);
+                res.push_back(p2.second);
+                p2.first--;
 
+                if(p2.first > 0)
+                    pq.push(p2);
+
+                pq.push(p);
+            }
         }
-    }
-    return res;
-    
+
+        return res;        
     }
 };
