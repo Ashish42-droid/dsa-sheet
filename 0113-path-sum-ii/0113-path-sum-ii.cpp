80ff5149a842801678a546ec40d43b61a0a0e0fc
Ashish42-droid <ashkumar91980@gmail.com>
2026-09-07 12:50:01 +0530

    Time: 0 ms (100%), Space: 20.7 MB (99.22%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -1,45 +1,31 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-    using node= TreeNode;
-    public:
+public:
+    using node = TreeNode;
+
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-     int sum = 0;
-     vector<vector<int>> res;
-     vector<int> diary;
-     fun (root,targetSum,sum,diary,res);
-     return res;
+        vector<vector<int>> res;
+        vector<int> diary;
+        fun(root, 0, targetSum, diary, res);
+        return res;
     }
 
-   void fun(node*root,int targetSum, int sum ,vector<int> &diary,vector<vector<int>> &res)
-    {
-        if (root == nullptr)
-        return ;
-        sum+= root->val;
-         diary.push_back(root->val);
-       
-        if (root->left == nullptr && root ->right == nullptr){
-            if (sum == targetSum){
-               res.push_back(diary);
-               diary.pop_back();
-               return;
+    void fun(node* root, int sum, int targetSum, vector<int> &diary, vector<vector<int>> &res) {
+        if (root == nullptr) return;
 
+        sum += root->val;
+        diary.push_back(root->val);
+
+        if (root->left == nullptr && root->right == nullptr) {
+            if (sum == targetSum) {
+                res.push_back(diary);  // store full path
             }
+            diary.pop_back(); // backtrack
+            return;
         }
 
-        fun (root->left,targetSum,sum,diary,res);
-        fun (root->right,targetSum,sum,diary,res);
-        diary.pop_back();
-        return ;
+        fun(root->left, sum, targetSum, diary, res);
+        fun(root->right, sum, targetSum, diary, res);
 
+        diary.pop_back(); // backtrack after exploring children
     }
 };
